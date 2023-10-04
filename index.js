0b2af6467515770c0bcf67bd2f7d73f3725c2963
McleodHerritt <nmcleod2021@gmail.com>
2023-10-04 13:10:22 -0300

    added comments to shapes.test.js, shapes.js, cli.js, index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+// Importing the CLI class from the cli.js file located in the lib directory
 const CLI = require("./lib/cli.js");
 
+// Creating a new instance of the CLI class
 const cli = new CLI();
 
+// Running the CLI instance to start the user interaction
 cli.run();
